b32528533ad7bcf7a026be80e7b13cf44139767d
Ben Hall <benhall2121@yahoo.com>
2012-07-30 10:36:33 -0700

    benhall2121@yahoo.com first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rml::Application.config.secret_token = 'ba7a988d69450fec14dc5f77720d25dd9208fcaf392663aee795cd31a36d4260c97aa866fc4af8882acce5eb74396b1fdf90bf8eebf24e89cb7d8b5a5fa74126'
